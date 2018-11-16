e3dfb4f30072b6f518c2e96a495c5a81889af05a
ian schoenrock <ianschoenrock@ians-macbook-pro.local>
2018-11-16 09:37:59 -0500

    Initial Commit

---
+++ My-Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My-Calculator
+//
+//  Created by ian schoenrock on 11/16/18.
+//  Copyright © 2018 ian schoenrock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
